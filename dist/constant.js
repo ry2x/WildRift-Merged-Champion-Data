0474ca891b02c65949626a5b6c0970f9ab8e394f
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-04-23 02:47:07 +0900

    add : compiled files

---
+++ dist/constant.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TAG_WORDS = exports.LANE_MAPPING = void 0;
+exports.LANE_MAPPING = {
+    中路: 'Mid',
+    打野: 'Jungle',
+    单人路: 'Top',
+    辅助: 'Support',
+    射手: 'AD',
+};
+exports.TAG_WORDS = {
+    fighter: 'Fighter',
+    mage: 'Mage',
+    assassin: 'Assassin',
+    marksman: 'Marksman',
+    support: 'Support',
+    tank: 'Tank',
+};
