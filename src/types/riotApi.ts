5fce97cb7b7bc5af7b650bb40a24e7df2e0da4dd
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-04-23 02:44:30 +0900

    add : type of riotAPI

---
+++ src/types/riotApi.ts
@@ -0,0 +1,70 @@
+import { Roles } from './merged';
+
+/**
+ * The list of available patch in DataDragon.
+ */
+export type DDPatchList = string[];
+
+/**
+ * The type of data returned by the DataDragon for champions.
+ */
+export interface ChampionData {
+  type: string;
+  format: string;
+  version: string;
+  data: Record<string, Champion>;
+}
+
+export interface Champion {
+  version: string;
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: ChampionInfo;
+  image: ChampionImage;
+  tags: Roles[];
+  partype: string;
+  stats: ChampionStats;
+}
+
+export interface ChampionInfo {
+  attack: number;
+  defense: number;
+  magic: number;
+  difficulty: number;
+}
+
+export interface ChampionImage {
+  full: string;
+  sprite: string;
+  group: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export interface ChampionStats {
+  hp: number;
+  hpperlevel: number;
+  mp: number;
+  mpperlevel: number;
+  movespeed: number;
+  armor: number;
+  armorperlevel: number;
+  spellblock: number;
+  spellblockperlevel: number;
+  attackrange: number;
+  hpregen: number;
+  hpregenperlevel: number;
+  mpregen: number;
+  mpregenperlevel: number;
+  crit: number;
+  critperlevel: number;
+  attackdamage: number;
+  attackdamageperlevel: number;
+  attackspeedperlevel: number;
+  attackspeed: number;
+}
