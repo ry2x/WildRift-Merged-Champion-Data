d4044fc2ad042f713659c914602bb2a0a7636d5c
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-04-23 03:46:09 +0900

    add : list of not available in Wild Rift

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,8 @@
 import { Lanes } from './types/merged';
 
+/**
+ * The mapping of lane names from CN API to the corresponding Lanes type.
+ */
 export const LANE_MAPPING: Record<string, Lanes> = {
   中路: 'Mid',
   打野: 'Jungle',
@@ -8,6 +11,9 @@ export const LANE_MAPPING: Record<string, Lanes> = {
   射手: 'AD',
 } as const;
 
+/**
+ * The mapping of tags(roles) in Riot API.
+ */
 export const TAG_WORDS = {
   fighter: 'Fighter',
   mage: 'Mage',
@@ -16,3 +22,54 @@ export const TAG_WORDS = {
   support: 'Support',
   tank: 'Tank',
 } as const;
+
+/**
+ * The mapping of lanes for champions that are not available in Wild Rift.
+ */
+export const NO_WR_LANE: Record<string, Lanes[]> = {
+  Anivia: ['Mid'],
+  Aphelios: ['AD'],
+  Aurora: ['Mid', 'Top'],
+  Azir: ['Mid'],
+  Bard: ['Support'],
+  Belveth: ['Jungle'],
+  Briar: ['Mid', 'Jungle'],
+  Cassiopeia: ['Mid', 'Top'],
+  Chogath: ['Mid', 'Top'],
+  Elise: ['Jungle'],
+  Gangplank: ['Mid', 'Top'],
+  Hwei: ['Mid', 'Support'],
+  Illaoi: ['Top'],
+  Ivern: ['Jungle'],
+  Karthus: ['Mid', 'Jungle'],
+  Kled: ['Top'],
+  KogMaw: ['AD'],
+  KSante: ['Top'],
+  Leblanc: ['Mid'],
+  Malzahar: ['Mid'],
+  Mel: ['Mid'],
+  Naafiri: ['Mid', 'Jungle'],
+  Neeko: ['Mid', 'Support'],
+  Nidalee: ['Mid', 'Jungle'],
+  Nocturne: ['Jungle'],
+  Qiyana: ['Mid', 'Jungle'],
+  Quinn: ['Top', 'AD'],
+  RekSai: ['Jungle'],
+  Rell: ['Support'],
+  Renata: ['Support'],
+  Sejuani: ['Jungle'],
+  Shaco: ['Mid', 'Top', 'Jungle'],
+  Skarner: ['Jungle'],
+  Smolder: ['Top', 'AD'],
+  Sylas: ['Mid'],
+  TahmKench: ['Top', 'Support'],
+  Taliyah: ['Mid'],
+  Taric: ['Top', 'Support'],
+  Trundle: ['Top', 'Jungle'],
+  Udyr: ['Top', 'Jungle'],
+  Velkoz: ['Mid', 'Support'],
+  Xerath: ['Mid', 'Support'],
+  Yorick: ['Top'],
+  Zac: ['Top', 'Jungle'],
+  Zilean: ['Mid', 'Support'],
+};
