4581d86be87e6a0497a983d25d9081534a03076a
Ry2X <45420571+ry2x@users.noreply.github.com>
2026-02-25 07:32:45 +0900

    UPDATE: Update packages and config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended);
